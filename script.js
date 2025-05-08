3888421ebed51c5375227ca3dcf10cf05fc43ff4
Naresh Koirala <koiralanaresh10@gmail.com>
2025-05-08 08:26:59 -0600

    fixed mobile version and animation

--- script.js
+++ script.js
@@ -20,5 +20,22 @@ function AddStars() {
 
         stars.push(star);
     }
-    console.log(stars);
+
+    for (let i = 0; i < numDots * 30; i++) {
+        const randomX = Math.floor(Math.random() * $(window).width());
+        const randomY = Math.floor(Math.random() * $(window).height());
+        const randomDelay = Math.random() * 20; // Random delay between 0 and 2 seconds
+        const randomDuration = 1 + Math.random() * 9; // Random duration between 1 and 3 seconds
+    
+        let dot = $('<div>').addClass('blink').css({
+            top: `${randomY}px`,
+            left: `${randomX}px`,
+            'animation-delay': `${randomDelay}s`,
+            'animation-duration': `${randomDuration}s`
+        });
+    
+        dot.appendTo('body');
+        stars.push(dot);
+    }
+
 }
